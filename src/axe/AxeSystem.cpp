2d5823e7b4bf027cc7e38764adaeda0f9db8fa97
Tyson <tysonlt@gmail.com>
2019-02-07 22:45:39 +1100

    Update AxeSystem.cpp

--- src/axe/AxeSystem.cpp
+++ src/axe/AxeSystem.cpp
@@ -168,14 +168,14 @@ void AxeSystem::setChanging() {
 }
 
 bool AxeSystem::isValidPresetNumber(int preset) {
-  return preset >= 0 && preset <= MAX_PRESETS;
+  return preset >= 0 && preset <= (int) MAX_PRESETS;
 }
 
 bool AxeSystem::isValidSceneNumber(int scene) { 
 	return scene >= 0 && scene <= MAX_SCENES; 
 }
 
-void AxeSystem::byteToMidiBytes(byte number, byte *byte1, byte *byte2) {
+void AxeSystem::intToMidiBytes(int number, byte *byte1, byte *byte2) {
 	*byte1 = number % BANK_SIZE;
 	*byte2 = number / BANK_SIZE;
 }
