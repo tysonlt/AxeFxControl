b2f6bcf4ae72aab8f5b7eeda4698b22845901a79
Tyson Lloyd Thwaites <tysonlt@gmail.com>
2019-02-11 21:28:07 +1100

    All typedefs and forward declarations

---
+++ src/AxeTypes.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Arduino.h>
+
+typedef byte Channel;
+typedef unsigned EffectId;
+typedef int16_t PresetNumber;
+typedef int8_t SceneNumber;
+typedef byte EffectIndex;
+typedef byte Tempo;
+typedef unsigned long millis_t;
+typedef byte LooperStatus;
+
+struct Version {
+	byte major, minor;
+};
+
+enum LooperButton {
+	LooperRecord,
+	LooperPlay,
+	LooperUndo,
+	LooperOnce,
+	LooperReverse,
+	LooperHalfSpeed
+};
+
+class AxeEffect;
+class AxePreset;
+class AxeLooper;
+class AxeSystem;
